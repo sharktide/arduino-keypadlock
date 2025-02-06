226a1fced70d3b0ba0a169da9215a1f5e7d12fdf
sharktide <meherrihaan@gmail.com>
2025-02-06 09:24:59 -0500

    Create codes.h

---
+++ codes.h
@@ -0,0 +1,14 @@
+#ifndef PIN_MAPPINGS_H
+#define CODES_H
+
+const int DHT_SENSOR_PIN = 5;  // Pin for the DHT11 sensor
+
+// LCD pins standard 6-pin configuration 
+const str ITEM1 = '7'; //item 1 of lock code
+const str ITEM2 = 'A'; //item 2 of lock code
+const str ITEM3 = '6'; //item 3 of lock code
+const int ITEM4 = '4'; //item 4 of lock code
+
+
+
+#endif
