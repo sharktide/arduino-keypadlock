c07f69d010869f6eeeedeb59730dffe1f8e95b9c
sharktide <meherrihaan@gmail.com>
2025-02-06 11:06:19 -0500

    Create mappings.h

---
+++ mappings.h
@@ -0,0 +1,26 @@
+// mappings.h
+
+#ifndef MAPPINGS_H
+#define MAPPINGS_H
+
+// Pin mappings for LEDs
+#define GREEN 49   // Green LED pin
+#define RED 45     // Red LED pin
+#define BLUE 47    // Blue LED pin
+#define WHITE 43   // White LED pin
+
+// Pin mapping for lock servo
+#define LOCK_PIN 10  // Pin to control the lock servo
+
+// Pin mappings for the keypad (rows and columns)
+#define ROW1_PIN 9   // Row 1 pin
+#define ROW2_PIN 8   // Row 2 pin
+#define ROW3_PIN 7   // Row 3 pin
+#define ROW4_PIN 6   // Row 4 pin
+
+#define COL1_PIN 5   // Column 1 pin
+#define COL2_PIN 4   // Column 2 pin
+#define COL3_PIN 3   // Column 3 pin
+#define COL4_PIN 2   // Column 4 pin
+
+#endif
